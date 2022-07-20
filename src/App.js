826a23bd8d4e2bba6debaf25904cdc70bf5fdf7b
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-07-20 17:05:03 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -8,11 +9,10 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
 }
 
 export default App;
-
-// Update
